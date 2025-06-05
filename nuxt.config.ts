01f131cfb2582c59bf73cad7390bed342fcb116e
Martin Behrendts <info@novusidea.de>
2025-06-05 23:40:22 +0200

    Added site url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2024-11-01',
+    site: {
+        url: 'https://martin-skroch.de',
+    },
     app: {
         head: {
             htmlAttrs: {
